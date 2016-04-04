84572d887f680876efda078f635176a170f8e7ae
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2016-04-04 02:02:54 +0200

    Update user.js

--- meteor/server/user/user.js
+++ meteor/server/user/user.js
@@ -1,3 +1,3 @@
-Meteor.users.before.insert(function (userId, doc) {
-    //TODO: find if doc is google or facebook then format data
-});
+//Meteor.users.before.insert(function (userId, doc) {
+//    //TODO: find if doc is google or facebook then format data
+//});
