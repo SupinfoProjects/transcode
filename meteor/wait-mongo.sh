266ed4ecb8fa25fa0b1b9a39234ea16521011104
Wassim DHIF <wassimdhif@gmail.com>
2016-05-25 13:02:34 +0200

    wdhif/transcode updated

--- meteor/wait-mongo.sh
+++ meteor/wait-mongo.sh
@@ -13,11 +13,11 @@ members_js=`echo $(IFS=,; echo "${members[*]}")`
 js="rs.initiate({_id:'${REPLICA_SET_ID}',members:[${members_js}]});"
 
 until mongo --host "${REPLICA_SET_ID}/${PRIMARY_MEMBER},${SECONDARY_MEMBERS}" "${DATABASE}"; do
-  >&2 echo "mongo --host ${REPLICA_SET_ID}/${PRIMARY_MEMBER},${SECONDARY_MEMBERS} ${DATABASE}"
-  >&2 echo "Mongo is unavailable - sleeping"
+  >&2 echo -e "\033[1m mongo --host ${REPLICA_SET_ID}/${PRIMARY_MEMBER},${SECONDARY_MEMBERS} ${DATABASE}"
+  >&2 echo -e "\033[1m Mongo is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Mongo is up - executing command"
+>&2 echo -e "\033[1m Mongo is up - Starting meteor"
 chmod +x $METEORD_DIR/run_app.sh
 exec $METEORD_DIR/run_app.sh
